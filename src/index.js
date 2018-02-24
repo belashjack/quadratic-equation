50da1e08522c43dd3c5147daf7ae12a36f2fc854
Yauheni Belash <something@site.com>
2018-02-24 21:38:28 +0300

    Create function which solves quadratic equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let arrOfSquareRoots = [];
+  let equationArray = equation.split(' ');
+  let a = +equationArray[0], 
+      b = equationArray[3] === '-' ? -equationArray[4]: +equationArray[4], 
+      c = equationArray[7] === '-' ? -equationArray[8]: +equationArray[8];
+  let D = b*b - 4*a*c;
+  let squareRoot1 = Math.round((-b + Math.sqrt(D))/(2 * a));
+  arrOfSquareRoots.push(squareRoot1);
+  let squareRoot2 = Math.round((-b - Math.sqrt(D))/(2 * a));
+  arrOfSquareRoots.push(squareRoot2);
+  return arrOfSquareRoots.sort((a, b) => a - b);
 }
